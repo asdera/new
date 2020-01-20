d7d91bf9d2d76a9aa6eff6b7fc6a354ae142c6e7
Andrew Wang <poiuytandrew@gmail.com>
2020-01-20 02:05:26 -0500

    Updates

---
+++ precache-manifest.4eb5c5aa72299dcd7e084a056ec6bc6c.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "697e9bce0589f9f4324efce440742ef5",
+    "url": "/new/index.html"
+  },
+  {
+    "revision": "de0b818b19927162eb37",
+    "url": "/new/static/css/main.7e0d0560.chunk.css"
+  },
+  {
+    "revision": "75ea2236309600a49263",
+    "url": "/new/static/js/2.611d2b9b.chunk.js"
+  },
+  {
+    "revision": "b00aab3725fcc29112905a8e835e6134",
+    "url": "/new/static/js/2.611d2b9b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de0b818b19927162eb37",
+    "url": "/new/static/js/main.6332819e.chunk.js"
+  },
+  {
+    "revision": "544e22ac1db69f994f42",
+    "url": "/new/static/js/runtime-main.c38d68c5.js"
+  },
+  {
+    "revision": "3670aa493ee09e92c7b8e1e7c2f5b441",
+    "url": "/new/static/media/CaviarDreams.3670aa49.ttf"
+  },
+  {
+    "revision": "7b02976656930aebbd3eeff675508874",
+    "url": "/new/static/media/Genera-AltLight.7b029766.ttf"
+  },
+  {
+    "revision": "4e05c1d252c878c049694b6fe15c7033",
+    "url": "/new/static/media/acers.4e05c1d2.png"
+  },
+  {
+    "revision": "ba275aa7022dadb8a866798e03c36d06",
+    "url": "/new/static/media/bhoppers.ba275aa7.png"
+  },
+  {
+    "revision": "67f0bf09d18a1bf6a7204235d552781f",
+    "url": "/new/static/media/enterprise.67f0bf09.png"
+  },
+  {
+    "revision": "071ed568bf1716375a8b3b80ce9e7071",
+    "url": "/new/static/media/flipper.071ed568.png"
+  },
+  {
+    "revision": "3170dd100e041e90da5ca346b45adcc5",
+    "url": "/new/static/media/fortnite.3170dd10.png"
+  },
+  {
+    "revision": "adbc5e91dde7859cf9d42a1b232e985f",
+    "url": "/new/static/media/gridiron.adbc5e91.png"
+  },
+  {
+    "revision": "50c9a1062cf025adf5cd1acc146229cc",
+    "url": "/new/static/media/hexagon.50c9a106.png"
+  },
+  {
+    "revision": "6ca1f6d940f0e8808be0ccb0ec699eed",
+    "url": "/new/static/media/jamhacks.6ca1f6d9.png"
+  },
+  {
+    "revision": "1e6222735b2efba2ba70bec28716411d",
+    "url": "/new/static/media/neon.1e622273.png"
+  },
+  {
+    "revision": "a4f7a7a2905378be921e6e8a21fe1a48",
+    "url": "/new/static/media/north.a4f7a7a2.png"
+  },
+  {
+    "revision": "9a92969f56cc62e79d2878f369785fa3",
+    "url": "/new/static/media/qcsys.9a92969f.png"
+  },
+  {
+    "revision": "48bd1dafbdf3ba5c8314e33497046102",
+    "url": "/new/static/media/scorpion.48bd1daf.png"
+  },
+  {
+    "revision": "6de48bcccf579779d8e4d4ef20984608",
+    "url": "/new/static/media/shad.6de48bcc.png"
+  },
+  {
+    "revision": "04f8c0e33f32f3fac06580f300ea90ed",
+    "url": "/new/static/media/starterhacks.04f8c0e3.png"
+  },
+  {
+    "revision": "3875d3b33e902f94f2ed3c0777397a65",
+    "url": "/new/static/media/volleyball.3875d3b3.png"
+  }
+]);
